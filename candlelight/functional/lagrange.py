14caae6d0c03d10f6965872d8343fba93578d7c3
Luca Grementieri <lgrementieri@nextb.it>
2020-02-20 17:07:32 +0100

    :sparkles: :white_check_mark: Barycentric polynomials 1D (#4)
    :sparkles: :white_check_mark: Barycentric polynomials 1D (#4)

--- candlelight/functional/lagrange.py
+++ candlelight/functional/lagrange.py
@@ -9,16 +9,15 @@ def lagrange1d(
     n = value.size(0) - 1
     inverse_factorial = torch.exp(
         -torch.lgamma(torch.arange(n + 1, dtype=torch.float64) + 1)
-    )
+    ).float()
     a = (
         pow(n / (domain[1] - domain[0]), n)
         * inverse_factorial
         * inverse_factorial.flip(dims=(0,))
-    ).float()
+    )
     a[(n + 1) % 2 :: 2] *= -1
     c = a * value
     x = torch.linspace(domain[0], domain[1], n + 1, dtype=torch.float32)
-    x = x.view(*(1,) * input.ndim, -1)
     d = torch.unsqueeze(input.unsqueeze(dim=-1) - x, dim=-2)
     d = d.repeat(*(1,) * input.ndim, n + 1, 1)
     d[..., torch.arange(n + 1), torch.arange(n + 1)] = 1
