7b24813740c9568a271d4ca3c8fe1fb6f1e04c28
Luca Grementieri <lgrementieri@nextb.it>
2020-04-13 16:13:54 +0200

    Add layers subpackage

---
+++ candlelight/layers/__init__.py
@@ -0,0 +1,7 @@
+from .akima import Akima1D
+from .barycentric import Barycentric1D
+from .cubic import Cubic1D
+from .ispline import Ispline1D
+from .krogh import Krogh1D
+from .lagrange import Lagrange1D
+from .pchip import Pchip1D
---
+++ candlelight/layers/pchip.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+from candlelight.functional import pchip1d
+
+
+class Pchip1D(nn.Module):
+    def __init__(self, nodes: int, domain: Tuple[float, float] = (0, 1)):
+        super().__init__()
+        self.value = nn.Parameter(torch.zeros(nodes), requires_grad=True)
+        self.domain = domain
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return pchip1d(input, self.value, self.domain)
