3b16c770795925a96b84e35e6c3a058a61c9808c
Luca Grementieri <lgrementieri@nextb.it>
2020-04-13 16:41:53 +0200

    :fire: Delete Ispline

--- candlelight/functional/__init__.py
+++ candlelight/functional/__init__.py
@@ -1,7 +1,6 @@
 from .akima import akima
 from .barycentric import barycentric
 from .cubic import cubic
-from .ispline import ispline
 from .krogh import krogh
 from .lagrange import lagrange
 from .pchip import pchip
