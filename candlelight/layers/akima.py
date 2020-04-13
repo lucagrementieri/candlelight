02926035313c40d22e9cc4529848e38153b54cb6
Luca Grementieri <lgrementieri@nextb.it>
2020-04-13 17:43:51 +0200

    Improve memory consumption

--- candlelight/layers/akima.py
+++ candlelight/layers/akima.py
@@ -9,9 +9,10 @@ from candlelight.functional import akima
 class Akima(nn.Module):
     def __init__(self, nodes: int, domain: Tuple[float, float] = (0, 1)):
         super().__init__()
-        self.value = nn.Parameter(torch.linspace(domain[0], domain[1], nodes), requires_grad=True)
+        self.node = torch.linspace(domain[0], domain[1], nodes)
+        self.value = nn.Parameter(self.node.clone(), requires_grad=True)
         self.domain = domain
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         input = torch.clamp(input, self.domain[0], self.domain[1])
-        return akima(input, self.value, self.domain)
+        return akima(input, self.node, self.value)
