f9eff5deb4de905fff3c4f61faa25e59a674f2de
Luca Grementieri <lgrementieri@nextb.it>
2020-04-13 21:36:13 +0200

    :art: Black reformat

--- candlelight/layers/cubic.py
+++ candlelight/layers/cubic.py
@@ -9,7 +9,9 @@ from candlelight.functional import cubic
 class Cubic(nn.Module):
     def __init__(self, nodes: int, domain: Tuple[float, float] = (0, 1)):
         super().__init__()
-        default_values = torch.linspace(domain[0], domain[1], nodes, dtype=torch.float32)
+        default_values = torch.linspace(
+            domain[0], domain[1], nodes, dtype=torch.float32
+        )
         self.value = nn.Parameter(default_values, requires_grad=True)
         self.domain = domain
 
