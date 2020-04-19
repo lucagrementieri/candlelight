f2b63c328c36afece2b1e8b5dc90481f87aaea44
Luca Grementieri <lgrementieri@nextb.it>
2020-04-19 14:09:15 +0200

    :alembic: Test previous version time

--- candlelight/functional/linear.py
+++ candlelight/functional/linear.py
@@ -1,9 +1,26 @@
 from typing import Tuple
 
 import torch
+import torch.nn.functional as F
 
 
 def linear(
+        input: torch.Tensor, value: torch.Tensor, domain: Tuple[float, float] = (0, 1)
+) -> torch.Tensor:
+    input_shape = input.shape
+    input = 2 * (input.flatten() - domain[1]) / (domain[1] - domain[0]) + 1
+    input = torch.stack((input, torch.zeros_like(input)), dim=-1)
+    input = input.view(1, 1, -1, 2)
+    value = value.view(1, 1, 1, -1)
+    # noinspection PyArgumentList
+    interpolation = F.grid_sample(
+        value, input, padding_mode='border', align_corners=True
+    )
+    interpolation = interpolation.reshape(input_shape)
+    return interpolation
+
+
+def linear_g(
     input: torch.Tensor, value: torch.Tensor, domain: Tuple[float, float] = (0, 1)
 ) -> torch.Tensor:
     n = value.numel() - 1
