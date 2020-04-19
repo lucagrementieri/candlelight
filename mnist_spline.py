bdb79e51df68bee02c5f5fd88d1b3804ebba0e2e
Luca Grementieri <lgrementieri@nextb.it>
2020-04-19 14:22:30 +0200

    :alembic: 11 nodes

--- mnist_spline.py
+++ mnist_spline.py
@@ -20,7 +20,7 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
-        self.interp = Linear(51, (-5, 5))
+        self.interp = Linear(11, (-5, 5))
 
     def forward(self, x):
         x = self.conv1(x)
