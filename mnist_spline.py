244f0311957a58e237c05f6d3afb65ecdb995a0a
Luca Grementieri <lgrementieri@nextb.it>
2020-04-19 12:23:03 +0200

    :alembic: Use same linear interpolation

--- mnist_spline.py
+++ mnist_spline.py
@@ -20,20 +20,18 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
-        self.interp1 = Linear(51, (-5, 5))
-        self.interp2 = Linear(51, (-5, 5))
-        self.interp3 = Linear(51, (-5, 5))
+        self.interp = Linear(11, (-5, 5))
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.interp1(x)
+        x = self.interp(x)
         x = self.conv2(x)
-        x = self.interp2(x)
+        x = self.interp(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = self.interp3(x)
+        x = self.interp(x)
         x = self.dropout2(x)
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
@@ -190,9 +188,7 @@ def main():
         test(model, device, test_loader)
         scheduler.step()
 
-    print(model.interp1.value.data)
-    print(model.interp2.value.data)
-    print(model.interp3.value.data)
+    print(model.interp.value.data)
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
