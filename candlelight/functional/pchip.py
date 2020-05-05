ed5e2dd794bc3835e192bc3b536192eb7cb65344
Luca Grementieri <lgrementieri@nextb.it>
2020-05-05 23:32:42 +0200

    :zap: Optimize PCHIP

--- candlelight/functional/pchip.py
+++ candlelight/functional/pchip.py
@@ -3,6 +3,8 @@ from typing import Tuple
 import torch
 import torch.nn.functional as F
 
+from candlelight.vector_sampler import VectorSampler
+
 
 def endpoint_slope(delta1: torch.Tensor, delta2: torch.Tensor) -> torch.Tensor:
     d = (3 * delta1 - delta2) / 2
@@ -29,19 +31,23 @@ def pchip(
         )
     )
     d[1:-1] *= F.relu(torch.sign(delta[1:]) * torch.sign(delta[:-1]))
-    interval = torch.clamp((input - domain[0]) // h, 0, n - 1).long()
+
+    sampler = VectorSampler(input, domain, n)
     x = torch.linspace(
         domain[0], domain[1], n + 1, dtype=torch.float32, device=input.device
     )
-    distance_left = input - x[interval]
-    distance_right = input - x[1 + interval]
+    distance_left = input - sampler.get_left(x)
+    distance_right = distance_left - h
     squared_left = torch.pow(distance_left, 2)
     squared_right = torch.pow(distance_right, 2)
-    f = value[interval] * squared_right * (distance_left + h / 2)
-    f -= value[1 + interval] * squared_left * (distance_right - h / 2)
+    value_left = sampler.get_left(value)
+    value_right = sampler.get_right(value)
+    d_left = sampler.get_left(d)
+    d_right = sampler.get_right(d)
+    f = value_left * squared_right * (distance_left + h / 2)
+    f -= value_right * squared_left * (distance_right - h / 2)
     f *= 2 / h ** 3
     f += (
-        d[interval] * squared_right * distance_left
-        + d[1 + interval] * squared_left * distance_right
+        d_left * squared_right * distance_left + d_right * squared_left * distance_right
     ) / h ** 2
     return f
