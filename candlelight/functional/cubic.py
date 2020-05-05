56b4bd7beab4c9affb4d418462443c0ad18afd2e
Luca Grementieri <lgrementieri@nextb.it>
2020-05-05 23:17:45 +0200

    :sparkles: Add VectorSampler and optimize cubic

--- candlelight/functional/cubic.py
+++ candlelight/functional/cubic.py
@@ -2,6 +2,8 @@ from typing import Tuple
 
 import torch
 
+from candlelight.vector_sampler import VectorSampler
+
 
 def cubic(
     input: torch.Tensor, value: torch.Tensor, domain: Tuple[float, float] = (0, 1)
@@ -14,18 +16,23 @@ def cubic(
     d = 3 * (value[2:] - 2 * value[1:-1] + value[:-2]) / h ** 2
     d = torch.cat((torch.zeros(1), d, torch.zeros(1))).unsqueeze_(-1)
     z, _ = torch.solve(d, A)
-    z = z.squeeze_(-1)
 
-    interval = torch.clamp((input - domain[0]) // h, 0, n - 1).long()
+    sampler = VectorSampler(input, domain, n)
     x = torch.linspace(
         domain[0], domain[1], n + 1, dtype=torch.float32, device=input.device
     )
-    distance_left = input - x[interval]
-    distance_right = x[1 + interval] - input
+    distance_left = input - sampler.get_left(x)
+    distance_right = h - distance_left
     cubic_left = torch.pow(distance_left, 3)
     cubic_right = torch.pow(distance_right, 3)
-    f = z[interval] * cubic_right + z[1 + interval] * cubic_left
+
+    z_left = sampler.get_left(z)
+    z_right = sampler.get_right(z)
+    value_left = sampler.get_left(value)
+    value_right = sampler.get_right(value)
+
+    f = z_left * cubic_right + z_right * cubic_left
     f /= 6 * h
-    f += (value[1 + interval] / h - z[1 + interval] * h / 6) * distance_left
-    f += (value[interval] / h - z[interval] * h / 6) * distance_right
+    f += (value_right / h - z_right * h / 6) * distance_left
+    f += (value_left / h - z_left * h / 6) * distance_right
     return f
