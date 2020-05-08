1adab252f3cf142b09910624d301719fab82498a
Luca Grementieri <lgrementieri@nextb.it>
2020-05-08 21:56:52 +0200

    :sparkles: More stable krogh interpolation

--- tests/functional/test_krogh.py
+++ tests/functional/test_krogh.py
@@ -7,8 +7,10 @@ from candlelight.functional import krogh
 
 test_params = [
     (np.linspace(0.1, 0.9, 12), np.array([1, 4, 6, 9]), (0, 1)),
+    (np.linspace(0, 1, 50), np.arange(50), (0, 1)),
     (np.linspace(2.6, 5, 14), np.array([6, 1, 2.4, 3.2]), (2, 5)),
     (np.linspace(1, 2, 36).reshape(6, 6), np.array([0.1, 0.3, 0.2, 0.5, 0.7]), (0, 2)),
+    (np.linspace(0.1, 0.9, 12), np.array([0.1, 0.5, 2, -0.5, 0.7]), (0, 1)),
 ]
 
 
@@ -22,4 +24,6 @@ def test_krogh(input, value, domain):
     input_tensor = torch.tensor(input, dtype=torch.float32, device=device)
     node_tensor = torch.tensor(value, dtype=torch.float32, device=device)
     torch_result = krogh(input_tensor, node_tensor, domain).cpu().numpy()
+    print(torch_result)
+    print(numpy_result)
     assert np.allclose(numpy_result, torch_result)
